dbdc03fb2b8f62725fd12a128b8ad8f5aa02374f
Behlul Vardal <123592293+Behlul10@users.noreply.github.com>
2026-08-18 21:01:10 -0400

    Initial commit

---
+++ functions/run_python_file.py
@@ -0,0 +1,38 @@
+import os
+import subprocess
+from sys import stdout
+
+def run_python_file(
+    working_directory: str, file_path: str, args: list[str] | None = None
+) -> str:
+    try:
+
+        working_dir_abs = os.path.abspath(f'{working_directory}') # returns full path
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path)) #returns dir path for the file
+        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs #bool
+        if not valid_target_dir:
+            return(f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory')
+        if not os.path.isfile(target_dir):
+            return(f'Error: "{file_path}" does not exist or is not a regular file')
+        if not file_path.endswith('.py'):
+            return(f'Error: "{file_path}" is not a Python file')
+        command = ["python", target_dir]
+        if args:
+            command.extend(args)
+
+        result = subprocess.run(command, capture_output=True, text=True, cwd=working_dir_abs,timeout=30 )
+
+        output = []
+
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+        if not result.stdout and not result.stderr:
+            output.append("No output produced")
+        if result.stdout:
+            output.append(f"STDOUT:\n{result.stdout}")
+        if result.stderr:
+            output.append(f"STDERR:\n{result.stderr}")
+
+        return "\n".join(output)
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
